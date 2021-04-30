a40d778ba0271ba303d0cc47fa5cda4efa3cd0cb
Zeynep İnan <zeynp.inan3269@gmail.com>
2021-04-30 20:04:14 +0300

    Döngüler, Değer tip ve Referans tip

--- JavaCampIntro/src/JavaCampIntro/Main.java
+++ JavaCampIntro/src/JavaCampIntro/Main.java
@@ -40,6 +40,25 @@ public class Main {
 		System.out.println(krediler[i]);
 	
 	}
-	}
-
-}
+	
+	//DEÐER TÝP
+	int sayi1=10;
+	int sayi2=20;
+	sayi1=sayi2;
+	sayi2=100;
+	System.out.println(sayi1);
+	
+	//REFERANS TÝP
+	int[] sayilar1= {1,2,3,4,5};
+	int[] sayilar2= {10,20,30,40,50};
+	sayilar1=sayilar2;
+	sayilar2[0]=100;
+	System.out.println(sayilar1[0]);
+	
+    //DEÐER TÝP
+	String sehir1 ="Ankara";
+	String sehir2 ="Ýstanbul";
+	sehir1=sehir2;
+	sehir2="Ýzmir";
+	System.out.println(sehir1);
+	}}
